02c19a30123857bc43620e99967d587009155455
Michael Fanuel <mfanuel@Michaels-MacBook-Air.local>
2021-05-05 16:37:28 +0200

    added choleski

--- regularizedPicard.jl
+++ regularizedPicard.jl
@@ -14,8 +14,8 @@ K = A*A';
 invK = inv(K);
 
 # sampling matrix
-id = [1,7,16, 20];
-#id = 1:N;
+#id = [1,7,16, 20];
+id = 1:N;
 U = Diagonal(vec(ones(n,1)));
 U = U[:,id];
 
@@ -30,19 +30,16 @@ lambda = 1;
 epsilon = 1e-15;
 
 # initialization
-invM = K/lambda; 
-M = lambda *invK;
-sqrtM = sqrt(lambda)*sqrt(invK);
-sqrtinvM = sqrt(K)/sqrt(lambda);
 
-
+R = cholesky(K).U;
+Rinv = inv(R);
 obj = zeros(n_it,1);
 
 # iterations
 for i in 1:n_it
     D = U *inv(U'*(L+ epsilon*I)*U)*U' - inv(I + L);
     fL = a*L*D*L +L;
-    L = 0.5*sqrtinvM*( sqrt(I + 4*sqrtM*fL*sqrtM) -I )*sqrtinvM;
+    L = (0.5/lambda)*R'*( sqrt(I + 4*lambda*Rinv'*fL*Rinv) -I )*R;
     obj[i] = -log(det(U'*L*U))+log(det(I + L)) + lambda*tr(L*invK);
 end
 
@@ -51,9 +48,9 @@ end
 #print(obj')
 
 # When id = 1:N
-#L_exact = 0.5*(sqrt(I + 4*K/lambda) -I);
-#print("\n")
-#print(norm(L-L_exact)/norm(L))
+L_exact = 0.5*(sqrt(I + 4*K/lambda) -I);
+print("\n")
+print(norm(L-L_exact)/norm(L))
 
 plot(1:n_it, obj, xlabel = "iteration", ylabel = "objective value")
 
