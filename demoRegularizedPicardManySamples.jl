224fe2efff5d64cdb9d1f3d532ad31eaa8025651
Michael Fanuel <mrfanuel@hotmail.fr>
2021-05-07 10:00:49 +0200

    typo corrected

--- demoRegularizedPicardManySamples.jl
+++ demoRegularizedPicardManySamples.jl
@@ -2,7 +2,7 @@ using LinearAlgebra
 using Plots
 include("regularizedPicard.jl")
 
-print("Warning: we solve only the case where S includes all the dpp samples")
+## Warning: we solve only the case where S includes all the dpp samples
 
 # total number of points
 n = 100;
@@ -16,15 +16,15 @@ id1 = [1,7,16, 20];
 id2 = [3,9,12, 17];
 samples = [[id1];[id2]]; # array of arrays
 
-# number of iteration
-n_it = 1000;
+# max number of iteration
+it_max = 1000;
 tol = 1e-4;
 
 # regularization
 lambda = 1;
 
 # iterations
-X, obj, i_stop = regularizedPicard(K, samples, lambda, n_it ,tol)
+X, obj, i_stop = regularizedPicard(K, samples, lambda, it_max ,tol)
 
 # plotting objectives
 plot(1:i_stop, obj[1:i_stop], xlabel = "iteration", ylabel = "objective value")
