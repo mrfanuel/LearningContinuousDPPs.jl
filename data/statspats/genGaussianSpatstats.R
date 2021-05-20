d72e56478bb258ecad2b7d46a804530647880fe2
Michael Fanuel <mrfanuel@hotmail.fr>
2021-05-20 11:50:13 +0200

    added gitignore for convenience

---
+++ data/statspats/genGaussianSpatstats.R
@@ -0,0 +1,25 @@
+library(spatstat)
+library(stats)
+
+#For parallelization
+library(foreach)
+library(doSNOW)
+library(parallel)
+
+#source("MLE_DPP.R") #Load the DPP function, make sure it is in the working directory.
+
+###############################Exemples of using the function MLEDPP#################################
+#####On a rectangular window#####
+#Gauss
+rho0 = 100
+alpha0 = 0.05
+S_length = 1
+
+for (x in 1:10) {
+    S = simulate(dppGauss(lambda=rho0, alpha=alpha0, d=2), 1, W=owin(c(0,S_length), c(0,S_length)))
+    #plot(S)
+    GaussDPPsample = data.frame(S)
+    write.csv(GaussDPPsample,paste('data/statspats/samples/GaussDPPsample_alpha0_00p',toString(alpha0*100),'_rho0_',toString(rho0),'_nb_',toString(x),'.csv', sep = ""))
+}
+
+ 
