5f805c38a26cf2f8e7c90c08cf442caaee9435aa
Michael Fanuel <mrfanuel@hotmail.fr>
2021-05-20 12:08:59 +0200

    changed another file name

---
+++ algo/estimateMVJacobi.jl
@@ -0,0 +1,158 @@
+# import utilities
+using LinearAlgebra
+using Plots
+using CSV
+using KernelFunctions
+using Distributions
+using DataFrames
+
+# algo for regularized Picard
+include("regularizedPicard.jl")
+include("integralCorrelationKernelFunction.jl")
+include("approxCorrelationKernelMatrix.jl")
+
+function estimateDPP(s::Int64,n::Int64,p::Int64,kernel::String,nu::Float64,sigma::Float64,lambda::Float64,epsilon::Float64,it_max::Int64,tol::Float64,n_step_plot::Int64)
+    ######################################################################
+    # uniform sampling for approx. Fedholm det and loading DPP samples 
+    ######################################################################
+
+    # create an array of arrays
+    idDppSamples = Array{Int64,1}[];
+
+    # uniform sampling in the box [-1,1]^2
+    c = 1.;
+    totalSamples = rand(Uniform(-c,c), n,2);
+    unifSample = collect(1:n); # i.e., the set I
+
+    ## loading DPP samples
+    id_last_sample = n;
+    # gather the samples
+    for i = 0:(s-1)
+        # read files and specify that they start from first row
+        temp = CSV.File("data/dppy/samples/MultivariateJacobiOPE_sample"*string(i)*".csv"; header=false) |> Tables.matrix 
+        # temp is a matrix with 2 cols
+        # add it as a new entry in matSamples
+        id_temp = collect((id_last_sample+1):(id_last_sample + size(temp,1)));
+        id_last_sample = id_last_sample + size(temp,1);
+        push!(idDppSamples,id_temp);
+        totalSamples = [totalSamples; temp];
+    end
+
+    # construct Gaussian kernel
+
+    print("kernel type: ")
+
+    x = (totalSamples)'/sigma;
+    if kernel=="MaternKernel"
+        k = MaternKernel(;nu);
+        print("Matern kernel \n")
+    else kernel=="SqExponentialKernel"
+        k = SqExponentialKernel();
+        print("SqExponential kernel \n")
+    end
+
+    K = kernelmatrix(k, x) + epsilon *I ; # makes sure it is positive definite
+
+    ################################
+    # solve estimation problem
+    ################################
+    print("Picard iteration...")
+
+    # iterations
+    X, obj, i_stop = regularizedPicard(K, idDppSamples, unifSample, lambda, it_max ,tol)
+
+
+    # recover matrix C
+    invK = inv(K);
+    X = 0.5*(X+X');# makes sure it is symmetric
+    C = invK*X*invK; 
+    C = 0.5*(C+C');# makes sure it is symmetric
+
+    ###################################
+    # estimation of correlation kernel
+    ###################################
+    print("\n")
+    print("estimate  correlation kernel...")
+    
+    K_hat_mat = approxCorrelationKernelMatrix(C,p,c,totalSamples,k,sigma);
+
+    print("\n")
+
+    print("plotting...")
+    # for plotting
+    intensity = zeros(Float64, n_step_plot,n_step_plot);
+
+    t = 0
+    for i in 1:n_step_plot
+        for j in 1:n_step_plot
+            t += 1
+            x = -1 + 2*(i-1)/(n_step_plot-1);
+            y = -1 + 2*(j-1)/(n_step_plot-1);
+            v = [x y]';
+            #K_nv=kernelmatrix(k, x_n, v);
+            #intensity[i,j] = ((K_nv)'*K_hat_mat*K_nv)[1];
+            intensity[i,j] = integralCorrelationKernelFunction(v,v,K_hat_mat,totalSamples,k,sigma);
+        end
+    end
+
+    # to check positive definiteness
+    #eig_mat_intensity = eigvals(intensity);
+    #print("eigenvalues empirical correlation matrix...")
+    #print(eig_mat_intensity)
+    
+
+    # plotting intensity of the estimated process
+
+    #gr()
+    x_tics = (-1:2*(1/(n_step_plot-1)):1);
+    y_tics = x_tics;
+    display(heatmap(x_tics,
+    y_tics,
+    intensity,
+    c=cgrad([:blue, :white,:red, :yellow]),
+    xlabel="x",
+    ylabel="y",
+    title="estimated intensity"))
+
+    
+    for i = 0:(s-1)
+        temp = CSV.File("data/sample"*string(i)*".csv"; header=false) |> Tables.matrix 
+        display(plot!(temp[:,1], temp[:,2], seriestype = :scatter, legend = false))
+    end
+
+    savefig("figures/MVJacobi_plot_"*string(s)*"_dppSamples_50_points.pdf")
+    # plotting objectives
+    # start plotting from it number 10 to have a nicer plot
+    #display(plot(10:i_stop, obj[10:i_stop], xlabel = "iteration", ylabel = "objective value",
+    #title="convergence regularized Picard"))
+
+    # construct grid
+    n_step = 10;
+
+    X = Array{Float64,2}[];
+    eps = 0.1;
+    t = 0
+    for i in 1:n_step
+        for j in 1:n_step
+            t += 1
+            x = -1 + eps + (2-2*eps)*(i-1)/(n_step-1);
+            y = -1 + eps + (2-2*eps)*(j-1)/(n_step-1);
+            v = [x y];
+            push!(X,v);
+        end
+    end
+        
+    nb_pts_grid = n_step*n_step;
+    GramMatrix = zeros(Float64, nb_pts_grid,nb_pts_grid);
+
+    for i in 1:nb_pts_grid
+        for j in 1:nb_pts_grid
+            v_i = X[i,:][1]';
+            v_j = X[j,:][1]';
+            GramMatrix[i,j] = integralCorrelationKernelFunction(v_i,v_j,K_hat_mat,totalSamples,k,sigma);
+        end
+    end
+
+
+    return GramMatrix;
+end 
---
+++ demoEstimationMVJacobi.jl
@@ -0,0 +1,49 @@
+include("algo/estimateMVJacobi.jl")
+
+# number of DPP samples
+s = 4;
+
+# number of uniform samples for Fredholm
+n = 100 # is better;
+
+# number of uniform samples for correlation kernel
+p = 500;
+
+# type of kernel
+#kernel = "MaternKernel"
+kernel = "SqExponentialKernel"
+
+# Matern nu, only for Matern (Float64)
+#nu = 0.5; # Laplace
+#nu = 1.5; # linear times Laplace
+nu = 5/2.;
+
+# kernel bw (Float64)
+
+#sigma = 2.; # for Matern larger than SqExponentialKernel
+sigma = 0.5; # for SqExponentialKernel # good 0.5
+
+# regularization (Float64)
+
+# for SqExponentialKernel
+lambda =  1.# too large 2# too small 0.5; # good 1.
+
+# for Matern
+#lambda =  0.5; # more or less fine
+
+# regularizer for K positive definite
+epsilon = 1e-6; # especially for SqExponentialKernel
+
+# max number of iteration
+it_max = 5000;
+
+# relative objective tolerance
+tol = 1e-6;
+
+# Plotting number of grid points along x-axis
+n_step_plot = 100; 
+
+
+GramMatrix = estimateDPP(s,n,p,kernel,nu,sigma,lambda,epsilon,it_max,tol,n_step_plot);
+
+# heatmap(GramMatrix)
